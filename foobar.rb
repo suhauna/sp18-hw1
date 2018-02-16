8163f99f2d8c56a113cdd7af54b5e155bfc718be
Suhauna <suhauna@gmail.com>
2018-02-15 17:14:19 -0800

    homework 1 from suhauna

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,9 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map!{|item| item.to_i}
+    a.map!{|item| item + 2}
+    a.find_all{|item| item % 2 == 0 and item < 10}.sum
+    #a.find_all{|item| item < 10 }.inject(0){|sum,item| sum + item }
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a+b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort!
+  a.map{|item| item+1 }
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
@@ -49,4 +53,9 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  
+  word.chars.map!{|item| item.to_sym }.map!{|item| values[item] }.sum
+  #sum
+  #sum
+
 end
